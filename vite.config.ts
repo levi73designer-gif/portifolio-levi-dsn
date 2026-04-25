73f28e2f07528900af35f8cc47eebc6192034726
Levi.dsn <levi73designer@gmail.com>
2026-04-25 10:37:39 -0300

    fix: vercel.json rewrites for SPA + vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
 })
